4170bbefe6c4fe99a8a5ed4e5604daa97d2af1bf
Alberto Sousa <avss@cin.ufpe.br>
2021-04-24 10:55:28 -0300

    adiciona controle de movimento lateral

--- DinoPlat/Assets/Scripts/Player/PlayerMovementBehaviour.cs
+++ DinoPlat/Assets/Scripts/Player/PlayerMovementBehaviour.cs
@@ -5,13 +5,43 @@ using UnityEngine.InputSystem;
 
 namespace DinoPlat.Player
 {
-	/// <summary>
+    /// <summary>
     /// Component to control player movement, handling player input for walk and jump
     /// </summary>
-	public class PlayerMovementBehaviour : MonoBehaviour
-	{
-		public void OnMovement(InputAction.CallbackContext context){
-            Debug.Log("Input: " + context.ReadValue<Vector2>());
+    [RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
+    public class PlayerMovementBehaviour : MonoBehaviour
+    {
+        private const string SPEED_ANIM_PARAM = "Speed";
+        private Vector2 movementInput;
+        private Rigidbody2D _body;
+        private Animator _animator;
+        private SpriteRenderer _spriteRenderer;
+
+        private void OnEnable()
+        {
+            _body = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
-	}
+
+        /// <summary>
+        /// Stores player input to be used on fixed update
+        /// </summary>
+        /// <param name="context"></param>
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            movementInput = context.ReadValue<Vector2>();
+        }
+
+        private void FixedUpdate()
+        {
+            _body.velocity = new Vector2(movementInput.x, 0f);
+        }
+
+        private void Update()
+        {
+            _spriteRenderer.flipX = _body.velocity.x < 0;
+            _animator.SetFloat(SPEED_ANIM_PARAM, Mathf.Abs(_body.velocity.x));
+        }
+    }
 }
