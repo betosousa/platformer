5292b45aca25c38bce6d387489988a73261998c1
Alberto Sousa <avss@cin.ufpe.br>
2021-04-25 15:43:37 -0300

    adiciona evento de player dead

---
+++ DinoPlat/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DinoPlat.Player
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayerHealth : MonoBehaviour
+    {
+		private void OnEnable() {
+			GetComponent<Animator>().SetBool("IsDead", false);
+		}
+        public void Die()
+        {
+            GetComponent<Animator>().SetBool("IsDead", true);
+        }
+    }
+}
