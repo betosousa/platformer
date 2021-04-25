b414754de70389a9d0cceeaec74dde5c23156876
Alberto Sousa <avss@cin.ufpe.br>
2021-04-25 15:51:51 -0300

    refatora PlayerHeath e adiciona comentarios

--- DinoPlat/Assets/Scripts/Player/PlayerHealth.cs
+++ DinoPlat/Assets/Scripts/Player/PlayerHealth.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 namespace DinoPlat.Player
 {
+    /// <summary>
+    /// Component that controls if the player is dead
+    /// </summary>
     [RequireComponent(typeof(Animator))]
     public class PlayerHealth : MonoBehaviour
     {
-		private void OnEnable() {
-			GetComponent<Animator>().SetBool("IsDead", false);
-		}
-        public void Die()
+        private const string IS_DEAD_ANIM_PARAM = "IsDead";
+        private Animator _animator;
+
+        private void OnEnable()
         {
-            GetComponent<Animator>().SetBool("IsDead", true);
+            _animator = GetComponent<Animator>();
+            _animator.SetBool(IS_DEAD_ANIM_PARAM, false);
         }
+
+        /// <summary>
+        /// Triggers the death animation state
+        /// </summary>
+        public void Die() => _animator.SetBool(IS_DEAD_ANIM_PARAM, true);
+
     }
 }
